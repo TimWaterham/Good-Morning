8205b21474a136788f8cbab3b1c5d97901d228b9
Tim Waterham <99021474@mydavinci.nl>
2017-03-16 16:44:29 +0100

    Changed everything to work properly, rewritten some of the PHP to make sure it looks better and correct

--- index.php
+++ index.php
@@ -1,27 +1,51 @@
+
+<!-- PHP -->
+<?php 
+	$timeZone = date_default_timezone_set('Europe/Amsterdam');
+	$time = date("H:i:s");
+	$pretext = "It is currently $time"; 
+
+	if ($time >="06:00" && $time < "12:00") {
+		$timeday = "Good Morning, $pretext";
+		$className = "morning";
+	} elseif ($time >= "12:00" && $time < "18:00") {
+		$timeday = "Good Afternoon, $pretext";
+		$className ="afternoon";	
+	} elseif ($time >= "18:00" && $time < "00:00") {
+		$timeday = "Good Evening, $pretext";
+		$className = "evening";
+	} elseif ($time >= "00:00" && $time < "06:00") {
+		$timeday = "Good Night, $pretext";
+		$className = "night";
+	} else {
+		$timeday = "error, $pretext"; 
+
+	};
+
+ ?>
+
+<!-- PHP -->
+<!--HTML -->
 <!DOCTYPE html>
 <html>
-<head>
-<!-- Title -->
-	<title>Good Morning</title>
-	<link href="css/style.css" rel="stylesheet">
-</head>
-
-<body>
-
-<?php 
-$time = date("G");
-
-if ($time < 6) {
-	echo "Good night";
-} 	else if($time < 12){
-	echo "Good morning";
-}	else if($time < 18){
-	echo "Good day";
-}	else {
-	echo "Good evening";
-}
-
-?>
-
-</body>
+	<head>
+		<title>Good Morning</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	</head>
+	<body class = "<?php echo $className; ?>">
+		<script type="text/javascript"> 
+			var timeout = setTimeout(reload, 1000); 
+			function reload() {
+				$("#reload").load('index.php #reload', function(){
+					timeout = setTimeout(reload, 1000);
+				});
+			}
+		</script>
+		<div id="reload">
+				<?php
+					echo $timeday;	
+				?>
+		</div>
+	</body>
 </html>
+<!--HTML -->
